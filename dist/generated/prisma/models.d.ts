dc4377608830ab737f05fba383bfee9cf456b07d
unknown <leonardoff24@gmail.com>
2026-01-13 14:28:57 -0300

    excluindo lixos de pastas não utilizadas

--- dist/generated/prisma/models.d.ts
+++
@@ -1,7 +0,0 @@
-export type * from './models/Tenant';
-export type * from './models/User';
-export type * from './models/Appointment';
-export type * from './models/Block';
-export type * from './models/RecurringBlock';
-export type * from './commonInputTypes';
-//# sourceMappingURL=models.d.ts.map
